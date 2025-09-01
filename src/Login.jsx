a0da63ec2991840f19984f3408ed2d2180fa84be
unknown <aakashrajak289@gmail.com>
2025-09-01 16:17:56 +0530

    create login page and call login api

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,8 +1,59 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useState } from 'react'
 
 const Login = () => {
+  const [emailId, serEmailId] = useState("Simaran@gmail.com");
+  const [password, setPassword] = useState("Simran@123");
+
+  const handleLogin = async () => {
+    try {
+      const res = await axios.post("http://localhost:3000/login", {
+        emailId,
+        password
+      }, { withCredentials: true })
+
+      console.log(res);
+    } catch (err) {
+      console.log(err.message)
+    }
+
+  }
   return (
-    <div>Login</div>
+    <div className='flex justify-center my-10'>
+      <div className="card bg-base-100 w-96 shadow-sm">
+        <div className="card-body">
+          <h2 className="card-title justify-center">Login</h2>
+          <div>
+            <label className="form-control w-full max-w-xs">
+              <div className="label mb-1">
+                <span className="label-text">Email Id</span>
+              </div>
+              <input
+                type="text"
+                value={emailId}
+                className="input input-rounded rounded-md max-w-xs w-full mb-3"
+                onChange={(e) => { serEmailId(e.target.value) }}
+              />
+            </label>
+            <label className="form-control w-full max-w-xs ">
+              <div className="label mb-1">
+                <span className="label-text">Password</span>
+              </div>
+              <input
+                type="text"
+                value={password}
+                className="input input-rounded rounded-md max-w-xs w-full"
+                onChange={(e) => { setPassword(e.target.value) }}
+              />
+            </label>
+          </div>
+          <div className="card-actions justify-center m-2">
+            <button className="btn btn-primary" onClick={handleLogin}>Login</button>
+          </div>
+        </div>
+      </div>
+
+    </div>
   )
 }
 
